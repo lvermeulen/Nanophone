72cb2150d07e936e8b58b10831b07a5dbd03c3cf
Vermeulen, Luk <Luk.Vermeulen@esko.com>
2016-05-28 23:08:46 +0200

    implemented WebApiRegistryConsumer

---
+++ src/Nanophone.RegistryConsumer.WebApi/WebApiRegistryConsumer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using Nanophone.Core;
+
+namespace Nanophone.RegistryConsumer.WebApi
+{
+    public class WebApiRegistryConsumer : IRegistryConsumer
+    {
+        public Uri Start(string serviceName, string version)
+        {
+            var uri = DnsHelper.GetNewLocalUri();
+            var selfHostConfiguration = new HttpSelfHostConfiguration(uri);
+
+            selfHostConfiguration.Routes.MapHttpRoute(
+                "API Default", "{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+
+            var server = new HttpSelfHostServer(selfHostConfiguration);
+            server.OpenAsync().Wait();
+            return uri;
+        }
+    }
+}
