b6d843c13a13bd83bb702ed43152741a1a4d5585
Vermeulen, Luk <Luk.Vermeulen@esko.com>
2016-05-28 22:51:54 +0200

    implemented NancyRegistryConsumer

--- src/Nanophone.Core/DnsHelper.cs
+++ src/Nanophone.Core/DnsHelper.cs
@@ -26,5 +26,23 @@ namespace Nanophone.Core
         {
             return GetLocalIpAddress().Replace(".", escape);
         }
+
+        private static int GetFreeTcpPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            return port;
+        }
+
+        public static Uri GetNewLocalUri(int port = 0)
+        {
+            port = port == 0 ? GetFreeTcpPort() : port;
+            var uri = new Uri($"http://localhost:{port}");
+
+            return uri;
+        }
     }
 }
--- src/Nanophone.RegistryConsumer.Nancy/NancyRegistryConsumer.cs
+++ src/Nanophone.RegistryConsumer.Nancy/NancyRegistryConsumer.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
 using Nanophone.Core;
 
 namespace Nanophone.RegistryConsumer.Nancy
 {
     public class NancyRegistryConsumer : IRegistryConsumer
     {
+        private static HostConfiguration GetHostConfiguration()
+        {
+            return new HostConfiguration
+            {
+                UrlReservations = { CreateAutomatically = true }
+            };
+        }
+
+        private static void StartHost(Uri uri, HostConfiguration hostConfiguration)
+        {
+            while (true)
+            {
+                try
+                {
+                    var host = new NancyHost(uri, new DefaultNancyBootstrapper(), hostConfiguration);
+                    host.Start();
+                }
+                catch
+                {
+                    Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+                    Console.WriteLine($"{typeof(NancyRegistryConsumer).Name}: unable to allocate port");
+                }
+            }
+        }
+
         public Uri Start(string serviceName, string version)
         {
-            throw new NotImplementedException();
+            var uri = DnsHelper.GetNewLocalUri();
+            var hostConfiguration = GetHostConfiguration();
+            StartHost(uri, hostConfiguration);
+
+            return uri;
         }
     }
 }
