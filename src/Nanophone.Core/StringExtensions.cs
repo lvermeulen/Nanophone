cbd78cc7b3748090b51a1222ac3c180686735d88
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-06-03 22:44:54 +0200

    added StringExtensions tests

--- src/Nanophone.Core/StringExtensions.cs
+++ src/Nanophone.Core/StringExtensions.cs
@@ -8,6 +8,11 @@ namespace Nanophone.Core
     {
         public static string TrimStart(this string source, string trim, StringComparison stringComparison = StringComparison.Ordinal)
         {
+            if (source == null)
+            {
+                return null;
+            }
+
             string s = source;
             while (s.StartsWith(trim, stringComparison))
             {
---
+++ src/Nanophone.RegistryHost.ConsulRegistry.Tests/StringExtensionsShould.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Nanophone.Core;
+
+namespace Nanophone.RegistryHost.ConsulRegistry.Tests
+{
+    [TestFixture]
+    public class StringExtensionsShould
+    {
+        const string PREFIX = "prefix-";
+
+        [Test]
+        public void TrimStartSingle()
+        {
+            string s = $"{PREFIX}string";
+            Assert.AreEqual("string", s.TrimStart(PREFIX));
+        }
+
+        [Test]
+        public void TrimStartMultiple()
+        {
+            string s = $"{PREFIX}{PREFIX}string";
+            Assert.AreEqual("string", s.TrimStart(PREFIX));
+        }
+
+        [Test]
+        public void TrimStartNone()
+        {
+            string s = "string";
+            Assert.AreEqual("string", s.TrimStart(PREFIX));
+        }
+
+        [Test]
+        public void TrimStartEmpty()
+        {
+            string s = "";
+            Assert.AreEqual("", s.TrimStart(PREFIX));
+        }
+
+        [Test]
+        public void TrimStartNull()
+        {
+            string s = null;
+            Assert.AreEqual(null, s.TrimStart(PREFIX));
+        }
+    }
+}
