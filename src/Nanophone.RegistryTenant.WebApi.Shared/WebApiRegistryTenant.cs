f2df2b859e66f36ee24f8d76ff3dbac7ed916117
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-09 13:56:31 +0200

    add WebApi registry tenant

---
+++ src/Nanophone.RegistryTenant.WebApi.Shared/WebApiRegistryTenant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nanophone.Core;
+
+namespace Nanophone.RegistryTenant.WebApi
+{
+    public class WebApiRegistryTenant : IRegistryTenant
+    {
+        public Uri Uri { get; }
+
+        public WebApiRegistryTenant(Uri uri)
+        {
+            Uri = uri;
+        }
+    }
+}
