ff3b2febe615fabd5635a0294cbd1b9a9ec7b103
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-09 11:45:43 +0200

    add test projects

---
+++ test/Nanophone.Core.Tests/StringExtensionsShould.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Nanophone.Core.Tests
+{
+    public class StringExtensionsShould
+    {
+        const string PREFIX = "prefix-";
+
+        [Fact]
+        public void TrimStartSingle()
+        {
+            string s = $"{PREFIX}string";
+            Assert.Equal("string", s.TrimStart(PREFIX));
+        }
+
+        [Fact]
+        public void TrimStartMultiple()
+        {
+            string s = $"{PREFIX}{PREFIX}string";
+            Assert.Equal("string", s.TrimStart(PREFIX));
+        }
+
+        [Fact]
+        public void TrimStartNone()
+        {
+            string s = "string";
+            Assert.Equal("string", s.TrimStart(PREFIX));
+        }
+
+        [Fact]
+        public void TrimStartEmpty()
+        {
+            string s = "";
+            Assert.Equal("", s.TrimStart(PREFIX));
+        }
+
+        [Fact]
+        public void TrimStartNull()
+        {
+            string s = null;
+            Assert.Equal(null, s.TrimStart(PREFIX));
+        }
+    }
+}
