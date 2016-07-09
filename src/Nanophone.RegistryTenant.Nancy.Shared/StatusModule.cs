2f0ee51b235f1d14fee900cf552d81b7f655e669
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-09 13:13:58 +0200

    add Nancy registry tenant (waiting for Nancy -Pre to support netcore

---
+++ src/Nanophone.RegistryTenant.Nancy.Shared/StatusModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nanophone.RegistryTenant.Nancy.Logging;
+
+namespace Nanophone.RegistryTenant.Nancy
+{
+    public class StatusModule : NancyModule
+    {
+        private static readonly ILog s_log = LogProvider.For<StatusModule>();
+
+        public StatusModule()
+        {
+            Get["/status"] = param =>
+            {
+                s_log.Info("Status: OK");
+                return "OK";
+            };
+        }
+    }
+}
