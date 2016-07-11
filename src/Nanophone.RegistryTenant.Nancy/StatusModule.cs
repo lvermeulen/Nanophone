2dfea25176bef4eec3b377d32f2a33b3d17f62ee
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-11 22:35:23 +0200

    refactor for .net core

---
+++ src/Nanophone.RegistryTenant.Nancy/StatusModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nanophone.RegistryTenant.Nancy.Logging;
+
+namespace Nanophone.RegistryTenant.Nancy
+{
+    public class StatusModule : NancyModule
+    {
+        private static readonly ILog s_log = LogProvider.For<StatusModule>();
+
+        public StatusModule()
+        {
+            Get["/status"] = (parameters, cancellationToken) =>
+            {
+                s_log.Info("Status: OK");
+                return Task.FromResult("OK");
+            };
+        }
+    }
+}
