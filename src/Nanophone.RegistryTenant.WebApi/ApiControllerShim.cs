633c7f0a8e0075d458ee906976ae9f80d5b92c96
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-11 22:58:20 +0200

    add ApiControllerShim

---
+++ src/Nanophone.RegistryTenant.WebApi/ApiControllerShim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if NETSTANDARD1_6
+using Microsoft.AspNetCore.Mvc;
+#else
+using System.Web.Http;
+#endif
+
+namespace Nanophone.RegistryTenant.WebApi
+{
+    public class ApiControllerShim
+#if NETSTANDARD1_6
+        : Controller
+#else
+        : ApiController
+#endif
+    { }
+}
