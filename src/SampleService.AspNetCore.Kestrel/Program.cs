df9898c76da5275af5c1113e502733defe8add4b
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-12 20:07:39 +0200

    add SampleService.AspNetCore.Kestrel

---
+++ src/SampleService.AspNetCore.Kestrel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Nanophone.Core;
+using Nanophone.RegistryHost.ConsulRegistry;
+using Nanophone.RegistryTenant.WebApi;
+using NLog;
+
+namespace SampleService.AspNetCore.Owin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const bool USING_FABIO = false;
+            const bool IGNORE_CRITICAL_SERVICES = true;
+
+            var log = LogManager.GetCurrentClassLogger();
+            log.Debug($"Starting {typeof(Program).Namespace}");
+
+            const int PORT = 9030;
+            string url = $"http://localhost:{PORT}/";
+            var serviceRegistry = new ServiceRegistry();
+            var consulConfiguration = USING_FABIO
+                ? new ConsulRegistryHostConfiguration { IgnoreCriticalServices = IGNORE_CRITICAL_SERVICES, FabioUri = new Uri("http://localhost:9999") }
+                : new ConsulRegistryHostConfiguration { IgnoreCriticalServices = IGNORE_CRITICAL_SERVICES };
+            serviceRegistry.Start(new WebApiRegistryTenant(new Uri(url)), new ConsulRegistryHost(consulConfiguration),
+                USING_FABIO ? "values v1.7-pre" : "values", "1.7-pre", relativePaths: new[] { "/values" });
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://*:{PORT}")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
