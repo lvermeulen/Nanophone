fe7809f299b0b24c983101589d56ba3d84c656db
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-13 22:13:27 +0200

    improve Consul critical service selection

---
+++ test/Nanophone.RegistryHost.ConsulRegistry.Tests/HealthCheckExtensionsShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Consul;
+using Xunit;
+
+namespace Nanophone.RegistryHost.ConsulRegistry.Tests
+{
+    public class HealthCheckExtensionsShould
+    {
+        [Fact]
+        public void IgnoreServicesWithoutServiceId()
+        {
+            var healthCheck = new HealthCheck { ServiceID = "" };
+            Assert.False(healthCheck.NeedsStatusCheck());
+        }
+
+        [Fact]
+        public void IgnoreSystemHealthChecks()
+        {
+            var healthCheck = new HealthCheck { CheckID = "serfHealth" };
+            Assert.False(healthCheck.NeedsStatusCheck());
+        }
+
+        [Fact]
+        public void IgnoreServicesInNodeMaintenance()
+        {
+            var healthCheck = new HealthCheck { CheckID = "_node_maintenance" };
+            Assert.False(healthCheck.NeedsStatusCheck());
+        }
+
+        [Fact]
+        public void IgnoreServicesInMaintenance()
+        {
+            var healthCheck = new HealthCheck { CheckID = "_service_maintenance:" };
+            Assert.False(healthCheck.NeedsStatusCheck());
+        }
+    }
+}
