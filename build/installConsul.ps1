de91b64f2ad146400c67ad882ffa79cd69515380
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-15 20:45:28 +0200

    add install, build & test scripts

---
+++ build/installConsul.ps1
@@ -0,0 +1,8 @@
+$consul_version = '0.6.4'
+    
+choco install 7zip.commandline
+$consul_zip = "consul_$($consul_version)_windows_386.zip"
+if (!(test-path "c:\consul_install")) {mkdir c:\consul_install}
+write-output "Downloading https://releases.hashicorp.com/consul/$consul_version/$consul_zip"
+if (!(test-path "c:\consul_install\$consul_zip")) {(new-object net.webclient).downloadfile("https://releases.hashicorp.com/consul/$consul_version/$consul_zip", "c:\consul_install\$consul_zip")}
+7z x -y c:\consul_install\$consul_zip -oc:\consul\consul.test
