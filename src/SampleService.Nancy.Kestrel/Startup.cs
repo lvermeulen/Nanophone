7791c03fef0423f65c1f676ba3ecd17ea00f5a6a
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-15 22:02:35 +0200

    add SampleService.Nancy.Kestrel

---
+++ src/SampleService.Nancy.Kestrel/Startup.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Nancy.Owin;
+using NLog.Extensions.Logging;
+
+namespace SampleService.Nancy.Kestrel
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .SetBasePath(env.ContentRootPath);
+
+            Configuration = builder.Build();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddNLog();
+
+            app.UseOwin(x =>
+            {
+                x.UseNancy(opt => opt.Bootstrapper = new SampleBootstrapper());
+            });
+        }
+    }
+}
