c72529b8a04f3e456cf2363678df9df615eecd8e
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-23 12:28:51 +0200

    refactor IRegistryHost interface

--- src/Nanophone.Core/IRegistryHost.cs
+++ src/Nanophone.Core/IRegistryHost.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace Nanophone.Core
+﻿namespace Nanophone.Core
 {
-    public interface IRegistryHost : IResolveServiceInstances
-    {
-        Task RegisterServiceAsync(string serviceName, string version, Uri uri, Uri healthCheckUri = null, IEnumerable<KeyValuePair<string, string>> keyValuePairs = null);
-        Task StartClientAsync();
-        Task KeyValuePutAsync(string key, object value);
-        Task<T> KeyValueGetAsync<T>(string key);
-        Task KeyValueDeleteAsync(string key);
-        Task KeyValueDeleteTreeAsync(string prefix);
-    }
+    public interface IRegistryHost : IManageServiceInstances, IResolveServiceInstances, IHaveKeyValues
+    { }
 }
