fd2ac91b2b12e83ef6727f0e28d89907613b2f42
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-23 11:49:54 +0200

    remove unused usings

--- samples/SampleService.AspNetCore.Kestrel/Controllers/ValuesController.cs
+++ samples/SampleService.AspNetCore.Kestrel/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SampleService.AspNetCore.Owin.Controllers
--- test/Nanophone.Core.Tests/UriExtensionsShould.cs
+++ test/Nanophone.Core.Tests/UriExtensionsShould.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Nanophone.Core.Tests
