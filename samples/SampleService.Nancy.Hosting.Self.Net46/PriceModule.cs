3d5007e4e4d15539cc85ba9df1e77d9af226977b
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-26 23:02:28 +0200

    add Nancy net46 sample

---
+++ samples/SampleService.Nancy.Hosting.Self.Net46/PriceModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace SampleService.Nancy.Hosting.Self.Net46
+{
+    public class PriceModule : NancyModule
+    {
+        public PriceModule()
+        {
+            Get("/price", parameters => "Hello, price");
+        }
+    }
+}
