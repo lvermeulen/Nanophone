d9b982fe844c132253b9131207b03a6f10ff9c31
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-26 23:02:28 +0200

    add Nancy net46 sample

---
+++ samples/SampleService.Nancy.Hosting.Self.Net46/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using Nanophone.Core;
+using Nanophone.RegistryHost.ConsulRegistry;
+using Nanophone.RegistryTenant.Nancy;
+
+namespace SampleService.Nancy.Hosting.Self.Net46
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int PORT = 8899;
+
+            var uri = new Uri($"http://localhost:{PORT}/");
+            using (var nancyHost = new NancyHost(uri))
+            {
+                nancyHost.Start();
+
+                //var consulRegistryHost = new ConsulRegistryHost();
+                //var serviceRegistry = new ServiceRegistry(consulRegistryHost);
+
+                //serviceRegistry.AddTenant(new NancyRegistryTenant(uri), "orders", "1.3")
+                //    .Wait();
+
+                Console.WriteLine($"Now listening on {uri}/price. Press enter to stop");
+                Console.ReadKey();
+            }
+        }
+    }
+}
