3a9b6bbf0ee9e9f7d9b27c5ebe160a2cd926728f
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-27 21:18:07 +0200

    add IResolveServicesCatalog

---
+++ src/Nanophone.Core/IAmAvailable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nanophone.Core
+{
+    public interface IAmAvailable
+    {
+        Task<bool> IsAvailable();
+    }
+
+    public static class IAmAvailableExtensions
+    {
+        public static IAmAvailable FromUri(Uri uri)
+        {
+            AvailableUri isAvailable = uri;
+            return isAvailable;
+        }
+    }
+}
---
+++ src/Nanophone.Core/IResolveServicesCatalog.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Nanophone.Core
+{
+    public interface IResolveServicesCatalog
+    {
+        Task<IDictionary<string, string[]>> GetServicesCatalogAsync();
+    }
+}
