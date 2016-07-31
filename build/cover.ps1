7a069256f6536378e1db8e67bb32de62bdc32f67
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-31 22:40:44 +0200

    add opencover script to appveyor.yml

---
+++ build/cover.ps1
@@ -0,0 +1,67 @@
+﻿param (
+	[string]$CoverallsRepoToken=$(throw "-CoverallsRepoToken is required."),
+    [string]$CoverFilter=$(throw "-CoverFilter is required.")
+)
+
+# run restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
+#Get-ChildItem -Path $PSScriptRoot\..\test -Filter project.json -Recurse | ForEach-Object { & dotnet restore $_.FullName 2>&1 }
+
+$alwaysFilter = "-[xunit*]* -[Microsoft*]* -[dotnet*]* -[NuGet*]* -[Newtonsoft*]* -[Consul*]* -[Nancy*]* -[csc]* -[Anonymously*]*"
+$filter = "$CoverFilter $alwaysFilter"
+
+#$packagesPath = "C:\Users\Luk\.nuget\packages"
+$packagesPath = "\packages"
+$opencoverPath = $packagesPath + "\OpenCover\4.6.519\tools\OpenCover.Console.exe"
+$coverallsPath = $packagesPath + "\coveralls.io\1.3.4\tools\coveralls.net.exe"
+$tempPath = "\temp"
+
+# create temp path
+if (-not (test-path $tempPath) ) {
+    md $tempPath | Out-Null
+}
+
+# remove temp subfolders
+Get-ChildItem -Path $tempPath -Directory | ForEach-Object { Remove-Item -Recurse $_.FullName }
+
+# run opencover
+Get-ChildItem -Path ..\test -Filter project.json -Recurse | ForEach-Object {
+
+    $path = "$tempPath\$($_.Directory.BaseName)"
+    md $path | Out-Null
+
+    $tempBinPath = $path + "\bin\"
+    $tempCoveragePath = $tempPath + "\coverage\"
+    $tempCoverageFileName = $tempCoveragePath + "coverage.xml"
+    $targetArgs = "`"test -o $tempBinPath $($_.FullName)`""
+
+    md $tempBinPath | Out-Null
+
+    if (-not (test-path $tempCoveragePath) ) {
+        md $tempCoveragePath | Out-Null
+    }
+
+    & $opencoverPath `
+        -register:user `
+        -target:"dotnet.exe" `
+        -targetargs:$targetArgs `
+        -searchdirs:$tempBinPath `
+        -output:$tempCoverageFileName `
+        -mergebyhash `
+        -mergeoutput `
+        -skipautoprops `
+        -returntargetcode `
+        -filter:$filter `
+        -hideskipped:Filter `
+        -oldstyle 
+
+}
+
+<#
+
+# upload to coveralls.io
+& $coverallsPath `
+    --opencover $tempCoverageFileName `
+    --full-sources `
+    --repo-token $CoverallsRepoToken
+
+#>
