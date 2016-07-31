8f4b5f43728643e046c17b729c84ca107c9a0180
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-31 19:30:46 +0200

    add StatusModule test

--- test/Nanophone.RegistryTenant.Nancy.Tests/StatusModuleShould.cs
+++ test/Nanophone.RegistryTenant.Nancy.Tests/StatusModuleShould.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Nancy;
+using Nancy.Testing;
 using Xunit;
 
 namespace Nanophone.RegistryTenant.Nancy.Tests
 {
     public class StatusModuleShould
     {
+        [Fact]
+        public async Task ReturnSuccess()
+        {
+            var browser = new Browser(new DefaultNancyBootstrapper());
+            var result = await browser.Get("/status", with => {
+                with.HttpRequest();
+            });
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Equal("OK", result.Body.AsString());
+        }
     }
 }
