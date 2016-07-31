efba90704c613136ed0163c5548e8d9bb6a504ea
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-31 18:18:52 +0200

    add tests for Nancy & WebApi tenants

---
+++ test/Nanophone.RegistryTenant.Nancy.Tests/NancyRegistryTenantShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nanophone.Core;
+using Nanophone.RegistryHost.InMemoryRegistry;
+using Xunit;
+
+namespace Nanophone.RegistryTenant.Nancy.Tests
+{
+    public class NancyRegistryTenantShould
+    {
+        [Fact]
+        public async Task BeRegistered()
+        {
+            var registryHost = new InMemoryRegistryHost();
+            var serviceRegistry = new ServiceRegistry(registryHost);
+
+            // add tenant
+            var uri = new Uri("http://localhost:1234");
+            var tenant = new NancyRegistryTenant(uri);
+            string serviceName = nameof(NancyRegistryTenantShould);
+            await serviceRegistry.AddTenant(tenant, serviceName, serviceName);
+
+            // register
+            var instance = (await registryHost.FindAllServicesAsync())
+                .FirstOrDefault(x => x.Name == serviceName);
+            Assert.Equal(uri.Host, instance.Address);
+
+            // dergister
+            await registryHost.DeregisterServiceAsync(instance.Id);
+            instance = (await registryHost.FindAllServicesAsync())
+                .FirstOrDefault(x => x.Name == serviceName);
+            Assert.Null(instance);
+        }
+    }
+}
---
+++ test/Nanophone.RegistryTenant.WebApi.Tests/WebApiRegistryTenantShould.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Nanophone.Core;
+using Nanophone.RegistryHost.InMemoryRegistry;
+using Xunit;
+
+namespace Nanophone.RegistryTenant.WebApi.Tests
+{
+    public class WebApiRegistryTenantShould
+    {
+        [Fact]
+        public async Task BeRegistered()
+        {
+            var registryHost = new InMemoryRegistryHost();
+            var serviceRegistry = new ServiceRegistry(registryHost);
+
+            // add tenant
+            var uri = new Uri("http://localhost:1234");
+            var tenant = new WebApiRegistryTenant(uri);
+            string serviceName = nameof(WebApiRegistryTenantShould);
+            await serviceRegistry.AddTenant(tenant, serviceName, serviceName);
+
+            // register
+            var instance = (await registryHost.FindAllServicesAsync())
+                .FirstOrDefault(x => x.Name == serviceName);
+            Assert.Equal(uri.Host, instance.Address);
+
+            // dergister
+            await registryHost.DeregisterServiceAsync(instance.Id);
+            instance = (await registryHost.FindAllServicesAsync())
+                .FirstOrDefault(x => x.Name == serviceName);
+            Assert.Null(instance);
+        }
+    }
+}
