e9d11b93a14bd1b128a1222b70971d89bae66aca
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-31 18:18:52 +0200

    add tests for Nancy & WebApi tenants

---
+++ test/Nanophone.RegistryTenant.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nanophone.RegistryTenant.WebApi.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c32a2818-bde0-45bf-936d-e61cc9bf4ee0")]
