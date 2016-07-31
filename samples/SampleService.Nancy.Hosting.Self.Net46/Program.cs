40d0a6fc5aa444a61285079fdac9f2d3e4e5bd1f
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-31 20:17:50 +0200

    complete Nancy sample for net46

--- samples/SampleService.Nancy.Hosting.Self.Net46/Program.cs
+++ samples/SampleService.Nancy.Hosting.Self.Net46/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Nancy.Hosting.Self;
 using Nanophone.Core;
 using Nanophone.RegistryHost.ConsulRegistry;
@@ -17,11 +18,11 @@ namespace SampleService.Nancy.Hosting.Self.Net46
             {
                 nancyHost.Start();
 
-                //var consulRegistryHost = new ConsulRegistryHost();
-                //var serviceRegistry = new ServiceRegistry(consulRegistryHost);
+                var consulRegistryHost = new ConsulRegistryHost();
+                var serviceRegistry = new ServiceRegistry(consulRegistryHost);
 
-                //serviceRegistry.AddTenant(new NancyRegistryTenant(uri), "orders", "1.3")
-                //    .Wait();
+                serviceRegistry.AddTenant(new NancyRegistryTenant(uri), "price", "1.3", keyValuePairs: new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("key", "value") })
+                    .Wait();
 
                 Console.WriteLine($"Now listening on {uri}/price. Press enter to stop");
                 Console.ReadKey();
