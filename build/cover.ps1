908e7bd0a0c9e4c51fe3a89a3c7accc1de01221d
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-08-02 21:34:07 +0200

    delete cover filter workaround

--- build/cover.ps1
+++ build/cover.ps1
@@ -6,10 +6,7 @@
 Write-Output "Starting code coverage with filter: $CoverFilter"
 
 $alwaysFilter = "-[xunit*]* -[Microsoft*]* -[dotnet*]* -[NuGet*]* -[Newtonsoft*]* -[Consul*]* -[Nancy*]* -[AngleSharp]* -[csc]* -[Anonymously*]*"
-$actualFilter = "+[Nano*]* -[*]*.Logging.* -[*Tests]*" # while appveyor doesn't correctly give the entire filter string in environment, use this actual filter
-Write-Output "Actually using filter: $actualFilter"
-#$filter = "$CoverFilter $alwaysFilter"
-$filter = "$actualFilter $alwaysFilter"
+$filter = "$CoverFilter $alwaysFilter"
 
 $packagesPath = $env:USERPROFILE + "\.nuget\packages"
 $opencoverPath = $packagesPath + "\OpenCover\4.6.519\tools\OpenCover.Console.exe"
