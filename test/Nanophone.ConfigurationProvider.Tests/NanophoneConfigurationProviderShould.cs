c87dd557435c0ce06f4ebcdf5010957fa01fe968
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-08-09 23:02:41 +0200

    add configuration provider tests

---
+++ test/Nanophone.ConfigurationProvider.Tests/NanophoneConfigurationProviderShould.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Nanophone.RegistryHost.InMemoryRegistry;
+using Xunit;
+
+namespace Nanophone.ConfigurationProvider.Tests
+{
+    public class NanophoneConfigurationProviderShould
+    {
+        private readonly NanophoneConfigurationProvider _provider;
+
+        public NanophoneConfigurationProviderShould()
+        {
+            var registryHost = new InMemoryRegistryHost();
+            registryHost.KeyValues = new List<KeyValuePair<string, object>>
+            {
+                new KeyValuePair<string, object>("key1", "value1"),
+                new KeyValuePair<string, object>("key2", "value2")
+            };
+
+            _provider = new NanophoneConfigurationProvider(() => registryHost);
+        }
+
+        [Fact]
+        public void TryGet()
+        {
+            string value;
+            Assert.True(_provider.TryGet("key1", out value));
+            Assert.Equal("value1", value);
+
+            Assert.True(_provider.TryGet("key2", out value));
+            Assert.Equal("value2", value);
+        }
+
+        [Fact]
+        public void Set()
+        {
+            _provider.Set("key3", "value3");
+            string value;
+            Assert.True(_provider.TryGet("key3", out value));
+            Assert.Equal("value3", value);
+        }
+
+        [Fact]
+        public void GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+        {
+            // TODO: add Keys & List to IHaveKeyValues
+            //Assert.True(false);
+        }
+    }
+}
