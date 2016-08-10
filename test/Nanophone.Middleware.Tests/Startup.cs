fa6ef8f3cbbcfb3981be7f9afc31c5a94c693c6e
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-08-10 21:46:47 +0200

    skip middleware test (not ready)

---
+++ test/Nanophone.Middleware.Tests/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Nanophone.Core;
+using Nanophone.RegistryHost.InMemoryRegistry;
+
+namespace Nanophone.Middleware.Tests
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, LoggerFactory loggerFactory)
+        {
+            var registryHost = new InMemoryRegistryHost
+            {
+                ServiceInstances = new List<RegistryInformation>
+                {
+                    new RegistryInformation
+                    {
+                        Name = nameof(NanophoneMiddlewareShould)
+                    }
+                }
+            };
+
+            app.UseNanophone(() => registryHost);
+        }
+    }
+}
