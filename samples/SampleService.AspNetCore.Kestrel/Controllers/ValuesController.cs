9e41022e6507b1e592dd99c5fc61e01e3572785a
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-08-15 20:48:37 +0200

    cleanup

--- samples/SampleService.AspNetCore.Kestrel/Controllers/ValuesController.cs
+++ samples/SampleService.AspNetCore.Kestrel/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace SampleService.AspNetCore.Owin.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] { "value1", "value2" };
         }
 
         // GET api/values/5
