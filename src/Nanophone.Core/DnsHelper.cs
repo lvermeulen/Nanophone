2f903fa43563053cae644f5028f29d9327e06944
Luk Vermeulen <luk.vermeulen@gmail.com>
2016-09-08 22:31:40 +0200

    add DnsHelper

---
+++ src/Nanophone.Core/DnsHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Nanophone.Core
+{
+    public static class DnsHelper
+    {
+        public static async Task<string> GetIpAddressAsync(bool ipv4 = true)
+        {
+            var hostEntry = await Dns.GetHostEntryAsync(string.Empty);
+            foreach (var address in hostEntry.AddressList)
+            {
+                if (ipv4 && address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return address.ToString();
+                }
+
+                if (!ipv4 && address.AddressFamily == AddressFamily.InterNetworkV6)
+                {
+                    return address.ToString();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ test/Nanophone.Core.Tests/DnsHelperShould.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Nanophone.Core.Tests
+{
+    public class DnsHelperShould
+    {
+        [Fact]
+        public async Task GetIpv4AddressAsync()
+        {
+            var address = await DnsHelper.GetIpAddressAsync();
+            Assert.NotEmpty(address);
+        }
+
+        [Fact]
+        public async Task GetIpv6AddressAsync()
+        {
+            var address = await DnsHelper.GetIpAddressAsync(ipv4: false);
+            Assert.NotEmpty(address);
+        }
+    }
+}
