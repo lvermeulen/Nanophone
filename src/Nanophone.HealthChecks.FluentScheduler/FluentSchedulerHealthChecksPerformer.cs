fde9c78e180cc2887a9d1723030958159455076a
Luk Vermeulen <luk.vermeulen@gmail.com>
2016-09-12 09:25:54 +0200

    add Nanophone.HealthChecks.FluentScheduler

---
+++ src/Nanophone.HealthChecks.FluentScheduler/FluentSchedulerHealthChecksPerformer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentScheduler;
+using Nanophone.Core;
+
+namespace Nanophone.HealthChecks.FluentScheduler
+{
+    public class FluentSchedulerHealthChecksPerformer : IPerformHealthChecks
+    {
+        private readonly Action _executeHealthCheck;
+        private readonly Lazy<HttpClient> _httpClient;
+        private readonly Registry _registry = new Registry();
+
+        public FluentSchedulerHealthChecksPerformer(Action executeHealthCheck = null)
+        {
+            _executeHealthCheck = executeHealthCheck;
+            _httpClient = new Lazy<HttpClient>();
+            JobManager.Initialize(_registry);
+            JobManager.JobException += ex => System.Diagnostics.Debug.WriteLine(ex.Exception.Message);
+        }
+
+        public Task AddHealthCheckAsync(HealthCheckInformation healthCheckInformation)
+        {
+            int totalSeconds = (int)healthCheckInformation.Interval.TotalSeconds;
+            _registry.Schedule(_executeHealthCheck ?? (() => ExecuteHealthCheckAsync(healthCheckInformation).Wait()))
+                .WithName(healthCheckInformation.Id)
+                .NonReentrant()
+                .ToRunNow()
+                .AndEvery(Math.Max(1, totalSeconds)).Seconds();
+
+            return Task.FromResult(0);
+        }
+
+        public Task RemoveHealthCheckAsync(HealthCheckInformation healthCheckInformation)
+        {
+            JobManager.RemoveJob(healthCheckInformation.Id);
+            return Task.FromResult(0);
+        }
+
+        public async Task<bool> ExecuteHealthCheckAsync(HealthCheckInformation healthCheckInformation)
+        {
+            var response = await _httpClient.Value.GetAsync(healthCheckInformation.Uri);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ test/Nanophone.HealthChecks.FluentScheduler.Tests/FluentSchedulerHealthChecksPerformerShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nanophone.RegistryHost.InMemoryRegistry;
+using Xunit;
+
+namespace Nanophone.HealthChecks.FluentScheduler.Tests
+{
+    public class FluentSchedulerHealthChecksPerformerShould
+    {
+        [Fact(Skip = "Not sure yet how FluentScheduler works")]
+        public async Task ExecuteHealthChecks()
+        {
+            bool isExecuted = false;
+            var host = new InMemoryRegistryHost
+            {
+                HealthChecksPerformer = new FluentSchedulerHealthChecksPerformer(() => isExecuted = true)
+            };
+            var checkId = await host.RegisterHealthCheckAsync(nameof(FluentSchedulerHealthChecksPerformerShould),
+                nameof(FluentSchedulerHealthChecksPerformerShould), new Uri($"http://{nameof(FluentSchedulerHealthChecksPerformerShould)}"), TimeSpan.FromSeconds(1));
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Assert.True(isExecuted);
+
+            await host.DeregisterHealthCheckAsync(checkId);
+        }
+    }
+}
