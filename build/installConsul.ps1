dec826acf6914659cd608f705771f9e926443648
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-01-15 10:00:52 +0100

    update consul version in install script

--- build/installConsul.ps1
+++ build/installConsul.ps1
@@ -1,8 +1,9 @@
-$consul_version = '0.7.0'
+$consul_version = '0.7.2'
     
 choco install 7zip.commandline
 $consul_zip = "consul_$($consul_version)_windows_386.zip"
-if (!(test-path "c:\consul_install")) {mkdir c:\consul_install}
+$consul_url = "https://releases.hashicorp.com/consul/$($consul_version)/$consul_zip"
+if (!(test-path "c:\consul_install")) {mkdir -Force c:\consul_install | Out-Null}
 write-output "Downloading https://releases.hashicorp.com/consul/$consul_version/$consul_zip"
-if (!(test-path "c:\consul_install\$consul_zip")) {(new-object net.webclient).downloadfile("https://releases.hashicorp.com/consul/$consul_version/$consul_zip", "c:\consul_install\$consul_zip")}
+if (!(test-path "c:\consul_install\$consul_zip")) {Invoke-WebRequest $consul_url -OutFile "c:\consul_install\$consul_zip"}
 7z x -y c:\consul_install\$consul_zip -oc:\consul\consul.test
