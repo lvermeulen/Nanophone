f6a14932906c8eda5c12686648fe404504ac027c
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-01-21 10:27:36 +0100

    install consul with chocolatey

--- build/installConsul.ps1
+++ build/installConsul.ps1
@@ -1,9 +1,3 @@
 $consul_version = '0.7.2'
-    
-choco install 7zip.commandline
-$consul_zip = "consul_$($consul_version)_windows_386.zip"
-$consul_url = "https://releases.hashicorp.com/consul/$($consul_version)/$consul_zip"
-if (!(test-path "c:\consul_install")) {mkdir -Force c:\consul_install | Out-Null}
-write-output "Downloading https://releases.hashicorp.com/consul/$consul_version/$consul_zip"
-if (!(test-path "c:\consul_install\$consul_zip")) {Invoke-WebRequest $consul_url -OutFile "c:\consul_install\$consul_zip"}
-7z x -y c:\consul_install\$consul_zip -oc:\consul\consul.test
+
+choco install consul
